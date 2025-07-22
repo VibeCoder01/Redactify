854c5a9c4c2cc88337943fe25671321056c3a371
Shaun Dunmall <sdunmall@gmail.com>
2025-07-22 16:01:08 +0000

    Could we get rid of logging "method". Add logging of Machine Name, if po

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,12 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const { ip, method, url, geo } = request;
+  const { ip, url, geo } = request;
   const userAgent = request.headers.get('user-agent');
   
   const logData = {
     message: "User access",
     timestamp: new Date().toISOString(),
-    method,
     url,
     ip,
     userAgent,
